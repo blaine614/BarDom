d29a9695d862b971a63aac52917b3d3fa42d6316
Trevor Parks <rexsteak@yahoo.com>
2014-06-10 13:08:45 -0400

    More damn tests

--- testProject/gen/com/example/testproject/R.java
+++ testProject/gen/com/example/testproject/R.java
@@ -1195,13 +1195,16 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_h=0x7f030018;
         public static final int activity_main=0x7f030019;
-        public static final int fragment_h=0x7f03001a;
-        public static final int fragment_main=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int activity_test=0x7f03001a;
+        public static final int fragment_h=0x7f03001b;
+        public static final int fragment_main=0x7f03001c;
+        public static final int fragment_test=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
         public static final int h=0x7f0c0000;
         public static final int main=0x7f0c0001;
+        public static final int test=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1248,6 +1251,7 @@ containing a value of this type.
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
         public static final int title_activity_h=0x7f0a0010;
+        public static final int title_activity_test=0x7f0a0011;
     }
     public static final class style {
         /** 
